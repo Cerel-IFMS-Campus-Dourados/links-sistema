c5da81f8f7ba1fe36a9d0c4f9cebf4f4fd3f805d
Cerel-IFMS-Campus-Dourados <cerel.dr@ifms.edu.br>
2025-02-17 14:55:31 -0400

    Add files via upload

--- js/pagina_servidor.js
+++ js/pagina_servidor.js
@@ -1,56 +1,18 @@
-// function enviarNome() {
-//     const nome = document.getElementById("nome-chamada").value;
-//     const mesa = document.getElementById("numero-mesa").value;
-//     const servidor = document.querySelector('input[name="servidor"]:checked');
-
-//     if (nome && mesa && servidor) {
-//         // Salva o nome no localStorage
-//         localStorage.setItem("nomeUsuario", nome);
-//         localStorage.setItem("nomeMesa", mesa);
-//         localStorage.setItem("servidorSelecionado", servidor.value);
-
-//         // Exibe uma confirmação de envio
-//         document.getElementById("confirmation").style.display = "block";
-
-//         // Limpa o campo de input
-//         document.getElementById("nome-chamada").value = "";
-//         document.getElementById("numero-mesa").value = "";
-//         servidor.checked = false;
-
-//         // Recarrega a página após 2 segundos para permitir o envio de outro nome
-//         setTimeout(function () {
-//             document.getElementById("confirmation").style.display = "none";
-//         }, 2000); // Esconde a confirmação após 2 segundos
-//     } else {
-//         alert("Por favor, insira um nome!");
-//     }
-
-// }
-
-const channel = new BroadcastChannel('comunicado_channel'); // Canal de comunicação
-
 function enviarNome() {
     const nome = document.getElementById("nome-chamada").value;
     const mesa = document.getElementById("numero-mesa").value;
     const servidor = document.querySelector('input[name="servidor"]:checked');
 
     if (nome && mesa && servidor) {
-        // Salva as informações no localStorage
+        // Salva o nome no localStorage
         localStorage.setItem("nomeUsuario", nome);
         localStorage.setItem("nomeMesa", mesa);
         localStorage.setItem("servidorSelecionado", servidor.value);
 
-        // Envia a mensagem para as outras abas via BroadcastChannel
-        channel.postMessage({
-            nomeUsuario: nome,
-            nomeMesa: mesa,
-            servidorSelecionado: servidor.value
-        });
-
         // Exibe uma confirmação de envio
         document.getElementById("confirmation").style.display = "block";
 
-        // Limpa os campos de input
+        // Limpa o campo de input
         document.getElementById("nome-chamada").value = "";
         document.getElementById("numero-mesa").value = "";
         servidor.checked = false;
@@ -60,6 +22,7 @@ function enviarNome() {
             document.getElementById("confirmation").style.display = "none";
         }, 2000); // Esconde a confirmação após 2 segundos
     } else {
-        alert("Por favor, insira um nome e complete todos os campos!");
+        alert("Por favor, insira um nome!");
     }
+
 }
